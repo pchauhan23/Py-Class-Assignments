5b67040061d3653c71fd28f8c5043c0195c1cd59
pchauhan23 <pankajchauhanbaginta@gmail.com>
2020-02-06 14:02:09 +0530

    init commit

---
+++ CAD6.py
@@ -0,0 +1,42 @@
+# Q1:- Print a box
+
+# print('''***********
+# ***********
+# ***********
+# ***********''')
+
+
+# Q2
+# print("*"*5)
+# print("*   *")
+# print("*   *")
+# print("*"*5)
+
+
+# # Q3
+# print("*")
+# print("*"*2)
+# print("*"*3)
+# print("*"*4)
+# print("*"*5)
+
+
+#Q4
+# u=input("Enter your name\n")
+# p=input("Enter your P.no\n")
+# e=input("Enter your Email\n")
+# l1=[u,p,e]
+# print("Your Name P.No and Email is:")
+# print(l1)
+
+
+#Q5
+# a='Hello'
+# print(a[0]*2+a[1]*2+a[2]*2+a[3]*2+a[4]*2)
+
+
+#Q6
+# a="Pankaj"
+# print((a[0])+(a[0]+a[1])+ (a[0]+a[1]*2)+(a[0]+a[1]*2+a[3])+(a))
+
+
