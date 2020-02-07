39688ba4f887be58de3c3ed986fa23c76f346f66
pchauhan23 <pankajchauhanbaginta@gmail.com>
2020-02-07 15:42:18 +0530

    init commit

---
+++ CAD8.py
@@ -0,0 +1,46 @@
+#Q1
+
+# import math
+# print(math.pi)
+
+
+#Q2
+
+# a=int(input('Enter a Value:- '))
+# b=int(input('Enter a value:- '))
+# from math import pow,sqrt
+# x=pow(a,b)
+# y=sqrt(a)
+# print(x)
+# print(y)
+
+
+#Q3
+
+
+# import random
+# a=random.randint(10000,99999)
+# print(a)
+
+
+#Q4
+
+
+# import random
+# movielist=['wakanda','city girls','brothers in the hood']
+# movieName=random.choice(movielist)
+# print(movieName)
+
+
+#Q5
+
+#a
+# import random
+# a=random.randint(0,9)
+# print(a)
+
+#b
+
+# import random
+# print("Random number is: ")
+# print(random.randrange(0,10,2))
