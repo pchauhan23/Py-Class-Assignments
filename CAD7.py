138c5e69111509da4136e0e1a51e2b133379352f
pchauhan23 <pankajchauhanbaginta@gmail.com>
2020-02-07 13:30:40 +0530

    init commit

---
+++ CAD7.py
@@ -0,0 +1,67 @@
+#Q1
+#a
+# number= 1+2*3/4.0
+# print(number)
+
+#b
+# reminder=11%3
+# print(reminder)
+
+#c
+# squared=7**2
+# print(squared)
+
+#d
+# cubed=2**3
+# print(cubed)
+
+#e
+# listRepeat=[1,2,3]*3
+# print(listRepeat)
+
+#Q2
+#a
+# a=['Pankaj','Chauhan']
+# a.append('Baginta')
+# print(a)
+# #b
+# a,b=(['Pankaj']*3),(['Baginta']*3)
+# print(a,b)
+
+#Q3
+
+# a=['Pankaj']
+# b=[' Chauhan']
+# print(a+b)
+
+#Q4
+
+# a,b=('Pankaj'),(' Chauhan')
+# print(a+b)
+
+#Q5
+
+# a=8
+# b=3
+# print(a>b)
+# print(a<b)
+# print(a<=b)
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+
+
+#Q6
+
+# a=input('Enter a value:- ')
+# b=input('Enter a Value:- ')
+# print(a<b)
+
+
+#Q7
+
+# x=12
+# y=13
+# print(x>10 and y>10)
+# print(x>14 and y>14)
+# print(x>10 and y>10)
